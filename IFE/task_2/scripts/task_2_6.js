74095910c29501697a27c854f16704a0f9f376cb
abcedfy <wuxifyao@outlook.com>
2016-06-16 14:09:03 +0800

    Update task_2_6.js

--- IFE/task_2/scripts/task_2_6.js
+++ IFE/task_2/scripts/task_2_6.js
@@ -13,59 +13,47 @@ function renderOutput() {
 	output.innerHTML = '';
 	divs.forEach(function(div) {
 		output.appendChild(div);
-		div.addEventListener('click', function(event){
-			var divClicked = event.target;
-			divs = divs.filter(function(ele) {
-				return ele != divClicked;
-			});
-			renderOutput();
-		}, false);
 	});
-}
-
-function unshift(event) {
-	var num = document.getElementById('input').value;
-	if(parseInt(num, 10) == num) {
-		var div = createDiv(num);
-		divs.unshift(div);
-		renderOutput();
-	}else {
-		alert("请输入一个数字！");
-	}
 }
 
 function push(event) {
 	var num = document.getElementById('input').value;
 	if(parseInt(num, 10) == num) {
 		var div = createDiv(num);
-		divs.push(div);
+		if(event.target === document.getElementById('unshift')) {
+			divs.unshift(div);
+		}else if(event.target === document.getElementById('push')) {
+			divs.push(div);
+		}
 		renderOutput();
 	}else {
 		alert("请输入一个数字！");
 	}
 }
 
-function shift(event) {
+function cut(event) {
 	if(divs.length === 0) {
 		alert("已经全部删完啦！");
 	}else {
-		divs.shift();
+		if(event.target === document.getElementById('shift')) {
+			divs.shift();
+		}else if(event.target === document.getElementById('pop')) {
+			divs.pop();
+		}
 		renderOutput();
-	}
-}
-
-function pop(event) {
-	if(divs.length === 0) {
-		alert("已经全部删完啦！");
-	}else {
-		divs.pop();
-		renderOutput();
+		return;
 	}
 }
 
 (function () {
-	document.getElementById('unshift').addEventListener('click', unshift, false);
+	document.getElementById('unshift').addEventListener('click', push, false);
 	document.getElementById('push').addEventListener('click', push, false);
-	document.getElementById('shift').addEventListener('click', shift, false);
-	document.getElementById('pop').addEventListener('click', pop, false);
+	document.getElementById('shift').addEventListener('click', cut, false);
+	document.getElementById('pop').addEventListener('click', cut, false);
+	document.getElementById('output').addEventListener('click', function(event) {
+		divs = divs.filter(function(ele) {
+			return ele !== event.target;
+		});
+		renderOutput();
+	}, false);
 })();

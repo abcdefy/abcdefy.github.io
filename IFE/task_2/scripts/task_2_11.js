525fe66b48570f003c3e8052fc9579512e53c62d
abcdefy <wuxifyao@outlook.com>
2016-07-01 10:31:58 +0800

    Update task_2_11.js

--- IFE/task_2/scripts/task_2_11.js
+++ IFE/task_2/scripts/task_2_11.js
@@ -1,70 +1,82 @@
 var tree = function() {
-	var root = document.getElementById('root');
+	var root = document.getElementById('root'),
+		current,
+		queue = [];
 	var timer = {
 		timerId: 0,
 		queue: [],
-		add: function(node) {
-			this.queue.push(node);
+		add: function(fn) {
+			this.queue.push(fn);
 		},
 		fire: function() {
-			if(this.timerId) return;
-			var current = timer.queue[0];
 			(function() {
-				current.style = 'background-color: white';
-				if(timer.queue.length > 0) {
-					current = timer.queue.shift();
-					current.style = 'background-color: mediumspringgreen';
+				if (timer.queue.length > 0) {
+					if (timer.queue[0]()) timer.queue.shift();
 					timer.timerId = setTimeout(arguments.callee, 350);
-				}
+				};
 			})();
 		},
 		stop: function() {
 			clearTimeout(this.timerId);
 			this.timerId = 0;
-			this.queue = [];
+			queue = [];
 			var divs = document.getElementsByTagName('div');
 			this.queue.forEach.call(divs, e => e.style = 'background-color: white');
 		}
 	};
-	function traverseDF(fn) {
+	function traverseAni() {
+		if (current) current.style = 'background-color: white';
+		current = queue.shift();
+		if (current) {
+			current.style = 'background-color: mediumspringgreen';
+			div = current;
+			return false;
+		}
+		return true;
+	}
+	function traverseDF() {
 		(function traverse(currentNode) {
-			timer.queue.forEach.call(currentNode.children, e => traverse(e));
-			fn(currentNode);
+			queue.forEach.call(currentNode.children, e => traverse(e));
+			queue.push(currentNode);
 		})(root);
 	}
-	function traverseBF(fn) {
+	function traverseBF() {
 		var currentNode = root,
 			array = [];
 		while(currentNode) {
-			if(currentNode.children) array.push(...currentNode.children);
-			fn(currentNode);
+			if (currentNode.children) array.push(...currentNode.children);
+			queue.push(currentNode);
 			currentNode = array.shift();
 		}
 	}
 	function choose(fn) {
 		var type = document.getElementsByTagName('select')[0].value;
-		if(type === 'traverseBF') {
-			traverseBF(fn);
-		}else if(type === 'traverseDF') {
-			traverseDF(fn);
+		if (type === 'traverseBF') {
+			traverseBF();
+		}else if (type === 'traverseDF') {
+			traverseDF();
 		}
 	}
 	return {
 		show: function() {
 			timer.stop();
-			choose(timer.add.bind(timer));
+			choose();
+			timer.add(traverseAni);
 			timer.fire();
 		},
 		contains: function(date) {
 			var i;
 			timer.stop();
-			choose(timer.add.bind(timer));
-			i = timer.queue.findIndex(e => e.innerText.match(/\S+/).join() === date);
-			if(i === -1) {
-				alert('search failed！')
+			choose();
+			i = queue.findIndex(e => e.innerText.match(/\S+/).join() === date);
+			if (i === -1) {
+				alert('Search failed！')
 				return false;
 			}
-			timer.queue = timer.queue.slice(0, i + 1);
+			queue = queue.slice(0, i + 1);
+			i = queue[i];
+			timer.add(traverseAni);
+			timer.add(e => i.style = 'background-color: firebrick');
 			timer.fire();
 		}
 	};
@@ -73,8 +85,8 @@ var tree = function() {
 (function() {
 	document.getElementById('charge').addEventListener('click', e => {
 		var b = e.target;
-		if(b.type !== 'button') return false;
-		if(b.value === 'show') tree.show();
-		if(b.value === 'contains') tree.contains(document.getElementsByTagName('input')[0].value);
+		if (b.type !== 'button') return false;
+		if (b.value === 'show') tree.show();
+		if (b.value === 'contains') tree.contains(document.getElementsByTagName('input')[0].value);
 	}, false);
 })();

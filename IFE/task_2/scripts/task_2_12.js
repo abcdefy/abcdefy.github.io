281a4de50f801d4898dd7c2e44a0d2dc035dcb0a
abcdefy <wuxifyao@outlook.com>
2016-07-06 10:59:20 +0800

    Update task_2_12.js
    写的一塌糊涂，我都不知道在写神马@_@

--- IFE/task_2/scripts/task_2_12.js
+++ IFE/task_2/scripts/task_2_12.js
@@ -1,5 +1,4 @@
 var tree = function(node) {
-	var root = node;
 	return {
 		traverseBF: function(node, fn) {
 			var currentNode = node,
@@ -29,29 +28,45 @@ var tree = function(node) {
 
 (function() {
 	var root = document.getElementById('root');
-	tree.traverseDF( root, node => node.classList.add('hidden') );
+
+	function set() {
+		tree.traverseDF(root, node => {
+			node.classList.add('hidden');
+			if ( node.children.length !== 0 ) {
+				node.classList.add('before1');
+			}
+		});
+	}
 
 	function expand(e) {
 		var clickedNode = e.target || e;
 		if (!clickedNode.children[0]) return false;
 		if (/hidden/.test(clickedNode.children[0].className)) {
-			[].forEach.call( clickedNode.children, node => node.className = '' );
+			clickedNode.classList.remove('before1');
+			clickedNode.classList.add('before2');
+			[].forEach.call( clickedNode.children, node => node.classList.remove('hidden') );
 		} else {
-			tree.traverseBF(clickedNode, node => node.className ='hidden');
-			clickedNode.className = '';
+			tree.traverseBF( clickedNode, node => {
+				node.classList.add('hidden');
+				if (node.children.length !== 0) {
+					node.classList.remove('before2');
+					node.classList.add('before1');
+				}
+			});
+			clickedNode.classList.remove('hidden');
 		}
 	}
 
 	function search(e) {
 		var data = document.getElementsByTagName('input')[0].value;
-		tree.traverseBF(root, node => node.className ='hidden');
+		set();
 		tree.traverseBF(root, function(node) {
 			if ( node.innerText.match(/\S+/).join() === data ) {
 				node.style.color = 'firebrick';
 				(function() {
 					if ( node !== root ) {
 						node = node.parentElement;
-						[].forEach.call( node.children, node => node.className ='' );
+						[].forEach.call( node.children, node => node.classList.remove('hidden') );
 						arguments.callee();
 					}
 				})();
@@ -60,6 +75,7 @@ var tree = function(node) {
 		});
 	}
 
+	set();
 	root.addEventListener('click', expand, false);
 	document.getElementsByTagName('button')[0].addEventListener('click', search);
 })();
